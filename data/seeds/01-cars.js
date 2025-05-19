50cfbdf1e3fe09913c4b1d44bb6a1d2c4ff4ab6e
Csquare21997 <biscuitxc@gmail.com>
2025-05-19 13:09:58 -0400

    1

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [{
+    vin:'11111111',
+    make:'toyota',
+    model:'prius',
+    mileage:125413,
+    title:'clean',
+    transmission:'manual',
+},
+{
+    vin:'222222222',
+    make:'toyota',
+    model:'corolla',
+    mileage:11500,
+    title:'salvage',
+},
+{
+    vin:'3333333',
+    make:'ford',
+    model:'focus',
+    mileage:50000,
+
+}]
+
+exports.seed = async function (knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+    }
+    
